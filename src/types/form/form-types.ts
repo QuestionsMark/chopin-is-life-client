1f4cc7091aa2e8f5413d55207cc5c30a5ed641ea
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-11-09 23:23:54 +0100

    types added

---
+++ src/types/form/form-types.ts
@@ -0,0 +1,9 @@
+export interface RegisterForm {
+    password: string;
+    passwordRepeat: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    rulesAccepted: boolean; 
+    // yearsOfPlayingPiano: number;
+}
