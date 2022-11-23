3b85902f3e5eb14899af72e694be1473b202ab6a
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-11-23 16:59:58 +0100

    section styles updated

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,26 @@
+import { Paragraph } from "../../components/common/Paragraph";
+import { Section } from "../../components/common/Section";
+
 export const Home = () => {
     return (
         <main className="main home">
-            home
+            <Section title="Nowości">
+                <Paragraph>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sapiente quia odit optio dolorem delectus, consequuntur nesciunt nisi exercitationem numquam quis quisquam velit ipsa aspernatur, et adipisci molestias. Error, provident? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio quas cum sequi fugiat eligendi, iste voluptas corporis. Quos quas quaerat dicta id quam ea ex enim itaque aliquam, inventore quisquam.
+                </Paragraph>
+            </Section>
+            <Section title="Ostatnie Wydarzenia Społecznościowe">
+                <Paragraph end>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sapiente quia odit optio dolorem delectus, consequuntur nesciunt nisi exercitationem numquam quis quisquam velit ipsa aspernatur, et adipisci molestias. Error, provident? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio quas cum sequi fugiat eligendi, iste voluptas corporis. Quos quas quaerat dicta id quam ea ex enim itaque aliquam, inventore quisquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident perspiciatis ex nam odit obcaecati suscipit cum delectus est quas dolor sed libero reiciendis hic iste, a maxime, magni fugit repudiandae.
+                </Paragraph>
+                <Paragraph>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sapiente quia odit optio dolorem delectus, consequuntur nesciunt nisi exercitationem numquam quis quisquam velit ipsa aspernatur, et adipisci molestias. Error, provident? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio quas cum sequi fugiat eligendi, iste voluptas corporis. Quos quas quaerat dicta id quam ea ex enim itaque aliquam, inventore quisquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident perspiciatis ex nam odit obcaecati suscipit cum delectus est quas dolor sed libero reiciendis hic iste, a maxime, magni fugit repudiandae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat voluptas, ut maxime cum architecto et veniam quidem nisi ipsum tempore suscipit eos eligendi, veritatis voluptatum, laboriosam molestiae eum ab dolore.
+                </Paragraph>
+            </Section>
+            <Section>
+                <div>siema</div>
+                <div>siema</div>
+            </Section>
         </main>
     );
 };
