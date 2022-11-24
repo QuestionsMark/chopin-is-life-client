13eaa018bb3ad3de53bb83cf5bf725905ce1e05a
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-11-24 15:09:13 +0100

    Image added

---
+++ src/components/common/Image.tsx
@@ -0,0 +1,18 @@
+import { HOST_ADDRESS } from "../../config/config";
+
+interface Props {
+    alt: string;
+    src: string;
+    className?: string;
+    isStatic?: boolean;
+}
+
+export const Image = ({ alt, src, className, isStatic }: Props) => {
+    return (
+        <img
+            src={isStatic ? src : `${HOST_ADDRESS}/file/${src}`}
+            alt={alt}
+            className={`img${className ? ' ' + className : ''}`}
+        />
+    )
+};
